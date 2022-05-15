8a29b3038436ccb62f6edb8d8f0b6d4655c63c0f
liujiangchuan <liujiangchuan@100tal.com>
2022-05-15 16:47:04 +0800

    feat: header

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const BlogPage: React.FC = ()=>{
+    return <h1>blog</h1>
+}
+
+export default BlogPage;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const HomePage: React.FC = ()=>{
+    return (
+        <h1>home</h1>
+    )
+}
+
+export default HomePage;
